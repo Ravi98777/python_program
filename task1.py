ed0362eb74f1ff7382dbf0a053bad963c84214cb
Ravi98777 <hisharmaji96@gmail.com>
2025-08-06 23:06:55 +0530

    program written

--- task1.py
+++ task1.py
@@ -1,11 +1,11 @@
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
+num1 = int(input("Enter the first number : "))
+num2 = int(input("Enter the second number : "))
 
 print("The sum is:", num1 + num2)
 print("The subtraction is:", num1 - num2)
 print("The multiplication is:", num1 * num2)
 
 if num2 != 0:
-    print("The division is:", num1 / num2)
+    print("The division is :", num1 / num2)
 else:
     print("Division by zero is not allowed.")
--- task2.py
+++ task2.py
@@ -1,3 +1,3 @@
-fname = input("Enter your  first name : ")
-lname = input("Enter your  last name : ")
+fname = input("Enter your  first name  : ")
+lname = input("Enter your  last name  : ")
 print("Hello,"+fname+" "+lname+"!welcome to the python program.")
