70a5b75bb532ea11df931116397ec89277086e56
Ravi Sharma <hisharmaji96@gmail.com>
2025-08-11 17:41:23 +0530

    Update assignment2_task1.py

--- assignment2_task1.py
+++ assignment2_task1.py
@@ -1,6 +1,13 @@
+ # Function to check if a number is even
 def check_odd_even(num):
-    return num%2 == 0
-num = int(input("Enter the num "))
-if(check_odd_even(num)):
-    print(num," number is an even")
-else: print(num," number is as odd")    
+    # If num % 2 equals 0, it's even; returns True for even, False for odd
+    return num % 2 == 0
+
+# Taking input from user and converting it to integer
+num = int(input("Enter the num: "))
+
+# Calling the function and checking if the number is even
+if check_odd_even(num):
+    print(num, "number is an even")  # Prints if number is even
+else:
+    print(num, "number is an odd")   # Prints if number is odd
